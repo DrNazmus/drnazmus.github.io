14217ad17df776f39ee7ceff3ec7eca6a3948adc
DrNazmus <nazmussakibian@gmail.com>
2026-01-21 21:51:07 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+window.addEventListener("scroll", () => {
+  document.querySelectorAll(".reveal").forEach(el => {
+    const top = el.getBoundingClientRect().top;
+    const height = window.innerHeight;
+
+    if (top < height - 100) {
+      el.classList.add("active");
+    }
+  });
+});
